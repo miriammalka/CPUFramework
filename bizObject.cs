d6f9784f242ad8227c8d00101a1e472f6497e34d
miriammalka <miriammalka1234@gmail.com>
2024-02-21 21:48:12 -0500

    Committing work

--- bizObject.cs
+++ bizObject.cs
@@ -5,24 +5,33 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
 
 namespace CPUFramework
 {
-    public class bizObject
+    public class bizObject: INotifyPropertyChanged
     {
         string _tablename = "";string _getsproc = ""; string _updatesproc = ""; string _deletesproc = "";
         string _primarykeyname = ""; string _primarykeyparamname = "";
         DataTable _datatable = new();
+        List<PropertyInfo> _properties = new();
 
-        public bizObject(string tablename)
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bizObject()
         {
-            _tablename = tablename;
-            _getsproc = tablename + "Get";
-            _updatesproc = tablename + "Update";
-            _deletesproc = tablename + "Delete";
-            _primarykeyname = tablename + "Id";
+            Type t = this.GetType();
+            _tablename = t.Name;
+            if(_tablename.ToLower().StartsWith("biz")) { _tablename = _tablename.Substring(3); }
+            _getsproc = _tablename + "Get";
+            _updatesproc = _tablename + "Update";
+            _deletesproc = _tablename + "Delete";
+            _primarykeyname = _tablename + "Id";
             _primarykeyparamname = "@" + _primarykeyname;
-
+            _properties = t.GetProperties().ToList<PropertyInfo>();
         }
         public DataTable Load(int primarykeyvalue)
         {
@@ -30,16 +39,70 @@ namespace CPUFramework
             SqlCommand cmd = SQLUtility.GetSQLCommand(_getsproc);
             SQLUtility.SetParamValue(cmd, _primarykeyparamname, primarykeyvalue);
             dt = SQLUtility.GetDataTable(cmd);
+            if(dt.Rows.Count > 0)
+            {
+                LoadProps(dt.Rows[0]);
+            }
             _datatable = dt;
             return dt;
+        }
+
+        private void LoadProps(DataRow dr)
+        {
+            foreach(DataColumn col in dr.Table.Columns)
+            {
+                SetProp(col.ColumnName, dr[col.ColumnName]);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            SqlCommand cmd = SQLUtility.GetSQLCommand(_deletesproc);
+            SQLUtility.SetParamValue(cmd, _primarykeyparamname, id);
+            SQLUtility.ExecuteSQL(cmd);
+        }
+
+        public void Delete()
+        {
+            PropertyInfo? prop = GetProp(_primarykeyname, true, false);
+            if(prop != null) 
+            {
+                object? id = prop.GetValue(this);
+                if(id != null)
+                {
+                    this.Delete((int)id);
+                }
+            }
         }
 
         public void Delete(DataTable datatable)
         {
             int id = (int)datatable.Rows[0][_primarykeyname];
-            SqlCommand cmd = SQLUtility.GetSQLCommand(_deletesproc);
-            SQLUtility.SetParamValue(cmd, _primarykeyparamname, id);
+            this.Delete((int)id);
+        }
+
+        public void Save()
+        {
+            SqlCommand cmd = SQLUtility.GetSQLCommand(_updatesproc);
+            foreach(SqlParameter param in cmd.Parameters)
+            {
+                var prop = GetProp(param.ParameterName, true, false);
+                if(prop != null)
+                {
+                    object? val = prop.GetValue(this);
+                    if(val == null) { val = DBNull.Value; }
+                    param.Value =  val;
+                }
+            }
             SQLUtility.ExecuteSQL(cmd);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                if(param.Direction == ParameterDirection.InputOutput)
+                {
+                    SetProp(param.ParameterName, param.Value);
+                }
+
+            }
         }
 
         public void Save(DataTable datatable)
@@ -50,6 +113,37 @@ namespace CPUFramework
             }
             DataRow r = datatable.Rows[0];
             SQLUtility.SaveDataRow(r, _updatesproc);
+        }
+
+        private PropertyInfo? GetProp(string propname, bool forread, bool forwrite)
+        {
+            propname = propname.ToLower();
+            if (propname.StartsWith("@")) { propname = propname.Substring(1); }
+
+            PropertyInfo? prop = _properties.FirstOrDefault(p => 
+            p.Name.ToLower() == propname 
+            && (forread == false || p.CanRead == true)
+            && (forwrite == false || p.CanWrite == true)
+            );
+            return prop;
+        }
+
+        private void SetProp(string propname, object? value)
+        {
+            var prop = GetProp(propname, false, true);
+            if (prop != null)
+            {
+                if(value == DBNull.Value)
+                {
+                    value = null;
+                }
+                prop.SetValue(this, value);
+            }
+        }
+
+        protected void InvokePropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
     }
 }
